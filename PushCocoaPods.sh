9af813bd837d4f668db67a2d1d734f28d8e2d75e
LCOpenSDK <openimoulife@163.com>
2021-08-25 16:00:14 +0800

    添加推送测试

---
+++ PushCocoaPods.sh
@@ -0,0 +1,30 @@
+#添加库至本地
+pod repo add CocoaPods https://github.com/CocoaPods/Specs.git 
+
+PODSPEC_FILE=""
+#从目录中查找podspec文件
+#参数1：文件路径
+function findSPodSpecFileInFolder() {
+	for file in `find "$(pwd)"`
+	do
+		extension="${file##*.}"
+		if [ "$extension" == "podspec" ] 
+		then
+        	PODSPEC_FILE=${file}
+			echo "*** 找到 ${PODSPEC_FILE} *** "
+		fi
+	done
+}
+
+#检查podsepc文件
+findSPodSpecFileInFolder
+
+if [[ ! -f ${PODSPEC_FILE} ]]; then
+	echo "***Error: podspec 文件不存在，请检查 ***"
+	exit
+fi
+
+#提交podspec文件至远端仓库
+echo "***开始Push到远端仓库 ***"
+pod trunk push ${PODSPEC_FILE} --allow-warnings --verbose --sources=https://github.com/CocoaPods/Specs.git 
+echo "*** 结束 ***"
