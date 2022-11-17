8e6ea8c10bc75dfa7973aac9bc1cdb30a8945fa4
yyg <yyg@yygdeMac-mini.local>
2022-11-17 15:39:28 +0800

    3.12.00 pod依赖LCOpenSDK

--- LeChangeNewDemo/Pods/Target Support Files/Pods-LeChangeDemo/Pods-LeChangeDemo-frameworks.sh
+++ LeChangeNewDemo/Pods/Target Support Files/Pods-LeChangeDemo/Pods-LeChangeDemo-frameworks.sh
@@ -178,6 +178,7 @@ code_sign_if_enabled() {
 if [[ "$CONFIGURATION" == "Debug" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KVOController/KVOController.framework"
+  install_framework "${PODS_ROOT}/../../Framework/LCOpenSDKDynamic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
@@ -189,6 +190,7 @@ fi
 if [[ "$CONFIGURATION" == "Release" ]]; then
   install_framework "${BUILT_PRODUCTS_DIR}/AFNetworking/AFNetworking.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/KVOController/KVOController.framework"
+  install_framework "${PODS_ROOT}/../../Framework/LCOpenSDKDynamic.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MBProgressHUD/MBProgressHUD.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/MJRefresh/MJRefresh.framework"
   install_framework "${BUILT_PRODUCTS_DIR}/Masonry/Masonry.framework"
